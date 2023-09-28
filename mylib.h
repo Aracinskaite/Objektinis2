dcf0b6215eeee32c60fb8b0b61da5ba36a76322d
Aracinskaite <144428697+Aracinskaite@users.noreply.github.com>
2023-09-28 15:19:35 +0300

    Add files via upload

--- mylib.h
+++ mylib.h
@@ -1,13 +1,3 @@
-#include <iostream>
-#include <iomanip>
-#include <string>
-#include <vector>
-#include <algorithm>
-
-
-
-using std::cout;
-using std::cin;
 using std::endl;
 using std::string;
 using std::vector;
@@ -16,9 +6,6 @@ using std::fixed;
 using std::left;
 using std::right;
 using std::setprecision;
-using std::numeric_limits;
-using std::streamsize;
-using std::cerr;
 
 struct studentas {
     string vardas, pavarde;
@@ -29,4 +16,3 @@ struct studentas {
 
 
 void mano_funkcija();
-
