8cc5f1bcfd1fa43a72849f80276f0e033f83f329
Aracinskaite <144428697+Aracinskaite@users.noreply.github.com>
2023-10-25 12:43:22 +0300

    Add files via upload

--- timer.h
+++ timer.h
@@ -4,20 +4,8 @@
 #include <iostream>
 #include <chrono>
 #include <vector>
+#include <algorithm>
 
-class Timer {
-  private:
-    std::chrono::time_point<std::chrono::high_resolution_clock> start;
-  public:
-    Timer() : start{std::chrono::high_resolution_clock::now()} {}
-    void reset() {
-      start = std::chrono::high_resolution_clock::now();
-    }
-    double elapsed() const {
-      return std::chrono::duration<double>
-(std::chrono::high_resolution_clock::now() - start).count();
-  }
-};
 
 
 class Timer1 {
