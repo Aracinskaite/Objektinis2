a44682b5a971b88be41ca7143c8e03b3bbffe380
Aracinskaite <144428697+Aracinskaite@users.noreply.github.com>
2023-11-26 21:03:25 +0200

    Add files via upload

--- studlib.h
+++ studlib.h
@@ -1,7 +1,7 @@
+#pragma once
 #ifndef MYLIB_H
 #define MYLIB_H
 
-#include <iostream>
 #include <iostream>
 #include <iomanip>
 #include <string>
@@ -11,9 +11,11 @@
 #include <fstream>
 #include <sstream>
 #include <ctime>
-#include <string>
+#include <list>
+#include <numeric>
 
 using std::cout;
+using std::list;
 using std::cin;
 using std::endl;
 using std::string;
@@ -33,69 +35,49 @@ using std::cerr;
 using std::ofstream;
 using std::invalid_argument;
 
-double medianossk(const std::vector<int>& pazymiai);
-double vidurkiosk(const std::vector<int>& pazymiai);
-
 class Studentas {
 private:
-    string vardas_;
-    string pavarde_;
-    vector<int> pazymiai_;
+    std::string vardas_, pavarde_;
+    std::vector<int> pazymiai_;
     int egzaminas_;
-    double rezultatas_;
-    double rezultatas1_;
+    float rezultatas_;
+    float rezultatas1_;
 
 public:
-    Studentas() : egzaminas_(0) {} // default konstruktorius
+    Studentas();
+    Studentas(const std::string& vardas, const std::string& pavarde, const std::vector<int>& pazymiai, int egzaminas);
 
-    void setVardas(const string& vardas) {vardas_ = vardas;}
-    void setPavarde(const string& pavarde) {pavarde_ = pavarde;}
-    void setPazymiai(const vector<int>& pazymiai) {pazymiai_ = pazymiai;}
-    void addPazymys(int pazymys) {
-        pazymiai_.push_back(pazymys);
-    }
-    void setEgzaminas(const int& egzaminas) {egzaminas_ = egzaminas;}
-    void setRezultatas(const double& rezultatas) {rezultatas_ = rezultatas;}
-    void setRezultatas1(const double& rezultatas1) {rezultatas1_ = rezultatas1;}
+    void generuoti();
+    double vidurkiosk() const;
+    double medianossk() const;
 
-    std::istream& IveskStud(std::istream&);
+    const string& getVardas() const { return vardas_; }
+    const string& getPavarde() const { return pavarde_; }
+    const vector<int>& getPazymiai() const { return pazymiai_; }
+    int getEgzaminas() const { return egzaminas_; }
+    float getRezultatas() const { return rezultatas_; }
+    float getRezultatas1() const { return rezultatas1_; }
 
-    void Rezultatusk() {rezultatas_ = 0.4 * vidurkiosk(pazymiai_) + 0.6 * egzaminas_;}
-    void Rezultatusk1() {rezultatas1_ = 0.4 * medianossk(pazymiai_) + 0.6 * egzaminas_;}
+    void setVardas(const string& vardas) { vardas_ = vardas; }
+    void setPavarde(const string& pavarde) { pavarde_ = pavarde; }
+    void setPazymiai(int pazymys) {pazymiai_.push_back(pazymys);}
+    void setEgzaminas(int egzaminas) { egzaminas_ = egzaminas; }
+    void setRezultatas(float rezultatas) { rezultatas_ = rezultatas; }
+    void setRezultatas1(float rezultatas1) { rezultatas1_ = rezultatas1; }
 
-    // Getter for rezultatas_
-    double getRezultatas() const {return rezultatas_;}
-    double getRezultatas1() const {return rezultatas1_;}
-    inline const string& getVardas() const {return vardas_;}
-    inline const string& getPavarde() const {return pavarde_;}
+    static void studFailas(int sarasas);
+    static void nuskaitymas(list<Studentas>& grupe, int sarasas);
+    static void isvedimas(const list<Studentas>& grupe, const string& filename);
+    static void surusiuoti(list<Studentas>& grupe, list<Studentas>& vargsiukai, const string& pagalRusiuoti);
+    static void isvedimasLaiko(double laikas1, double laikas2, double laikas3, double laikas4, double laikas5, int testsk, int sarasas);
+    static void mano_funkcija1();
+    static void mano_funkcija();
 
-    void clearPazymiai() { pazymiai_.clear(); }
-
-
-    /*
-    Studentas(std::istream& is);
-    inline std::string vardas() const { return vardas_; }    // get'eriai, inline
-    inline std::string pavarde() const { return pavarde_; }  // get'eriai, inline
-    double galBalas(double (*) (const std::vector<int>&) = mediana) const;  // get'eriai
-    std::istream& readStudent(std::istream&);  // set'eriai*/
+    ~Studentas();
 };
 
-bool compare(const Studentas&, const Studentas&);
-bool comparePagalPavarde(const Studentas&, const Studentas&);
-bool comparePagalEgza(const Studentas&, const Studentas&);
-
-void mano_funkcija();
-void mano_funkcija1();
-
-
-
-void studFailas(int sarasas);
-void nuskaitymas(std::vector<Studentas>& grupe, int sarasas);
-void isvedimas(const std::vector<Studentas>& grupe, const std::string& filename);
-void surusiuoti(std::vector<Studentas>& grupe, std::vector<Studentas>& vargsiukai, std::vector<Studentas>& galvociai, const std::string& pagalRusiuoti);
-Studentas GeneruotiStudentai(int& numeris);
-void isvedimasLaiko(double laikas1, double laikas2, double laikas3, double laikas4, double laikas5, int testsk, int sarasas);
-
 #endif // MYLIB_H
+
+
 
 
